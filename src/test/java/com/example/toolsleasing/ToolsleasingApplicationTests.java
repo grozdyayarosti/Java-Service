548cc5068dd28283fc7704f483faa85e24b58183
Semyon Zabolotko <semenzabolotko.ib@gmail.com>
2024-11-25 00:14:27 +0500

    Запустил голый проект без функционала

---
+++ src/test/java/com/example/toolsleasing/ToolsleasingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toolsleasing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsleasingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
